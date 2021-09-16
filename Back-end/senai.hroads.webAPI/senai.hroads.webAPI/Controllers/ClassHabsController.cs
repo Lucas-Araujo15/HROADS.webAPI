ac80b8ece6c3fcb6c5bb7388e19aee30ef99c545
Gustavo Borges de Souza <82384564+GustavoBorgesSouza@users.noreply.github.com>
2021-09-16 17:16:06 -0300

    CRUD Classhab feito, porém talvez necessite de melhorias

---
+++ Back-end/senai.hroads.webAPI/senai.hroads.webAPI/Controllers/ClassHabsController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using senai.hroads.webAPI.Domains;
+using senai.hroads.webAPI.Interfaces;
+using senai.hroads.webAPI.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai.hroads.webAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClassHabsController : ControllerBase
+    {
+        private IClassHabRepository _classHabRepository { get; set; }
+
+        public ClassHabsController()
+        {
+            _classHabRepository = new ClassHabRepository();
+        }
+
+        [HttpGet]
+        public IActionResult Listar()
+        {
+            try
+            {
+                return Ok(_classHabRepository.Listar());
+            }
+            catch (Exception erro)
+            {
+                return BadRequest(erro);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult BuscarPorId(int id)
+        {
+            try
+            {
+                return Ok(_classHabRepository.BuscarPorId(id));
+            }
+            catch (Exception erro)
+            {
+                return BadRequest(erro);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(ClassHab novaClassHab)
+        {
+            try
+            {
+                _classHabRepository.Cadastrar(novaClassHab);
+                return StatusCode(201);
+            }
+            catch (Exception erro)
+            {
+                return BadRequest(erro);
+            }
+        }
+
+        [HttpPut("{idAtualiza}")]
+        public IActionResult Atualizar(int idAtualiza, ClassHab classHabAtualizada)
+        {
+            try
+            {
+                _classHabRepository.Atualizar(idAtualiza, classHabAtualizada);
+                return StatusCode(204);
+            }
+            catch (Exception erro)
+            {
+                return BadRequest(erro);
+            }
+        }
+
+        [HttpDelete("{idDeleta}")]
+        public IActionResult Deletar(int idDeleta)
+        {
+            try
+            {
+                _classHabRepository.Deletar(idDeleta);
+                return StatusCode(204);
+            }
+            catch (Exception erro)
+            {
+                return BadRequest(erro);
+            }
+        }
+    }
+}
