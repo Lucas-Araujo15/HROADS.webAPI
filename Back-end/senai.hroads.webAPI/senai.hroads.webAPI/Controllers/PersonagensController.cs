7cdc5a239f86a49dba6ac64105e2c3eecf0f0019
Lucas Araujo <lucasaraujo4188@gmail.com>
2021-09-17 00:13:49 -0300

    Adiciona o CRUD de personagens

---
+++ Back-end/senai.hroads.webAPI/senai.hroads.webAPI/Controllers/PersonagensController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using senai.hroads.webAPI.Domains;
+using senai.hroads.webAPI.Interfaces;
+using senai.hroads.webAPI.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai.hroads.webAPI.Controllers
+{
+    [Produces("application/json")]
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonagensController : ControllerBase
+    {
+        IPersonagemRepository _PersonagemRepository { get; set; }
+
+        public PersonagensController()
+        {
+            _PersonagemRepository = new PersonagemRepository();
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_PersonagemRepository.ListarTodos());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok(_PersonagemRepository.BuscarPorId(id));
+        }
+
+        [HttpPost]
+        public IActionResult Post(Personagem personagemCadastrar)
+        {
+            _PersonagemRepository.Inserir(personagemCadastrar);
+            return StatusCode(201);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _PersonagemRepository.Deletar(id);
+            return NoContent();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, Personagem personagemAtual)
+        {
+            if (personagemAtual.IdClasse == 0 || personagemAtual.Nome == null || personagemAtual.CapVida == 0 || personagemAtual.CapMana == 0)
+            {
+                return BadRequest(
+                    new
+                    {
+                        mensagemErro = "Algumas informações não foram informadas!"
+                    }
+                );
+            }
+
+            Personagem personagemBuscado = _PersonagemRepository.BuscarPorId(id);
+
+            if(personagemBuscado != null)
+            {
+                _PersonagemRepository.Atualizar(id, personagemAtual);
+                return Ok();
+            }
+
+            return NotFound(
+                    new
+                    {
+                        mensagemErro = "Personagem não encontrado!",
+                        codErro = true
+                    }
+                );
+        }
+    }
+}
---
+++ Back-end/senai.hroads.webAPI/senai.hroads.webAPI/Repositories/PersonagemRepository.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using senai.hroads.webAPI.Contexts;
+using senai.hroads.webAPI.Domains;
+using senai.hroads.webAPI.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai.hroads.webAPI.Repositories
+{
+    public class PersonagemRepository : IPersonagemRepository
+    {
+        HroadsContext ctx = new HroadsContext();
+        public void Atualizar(int idPersonagem, Personagem novoPersonagem)
+        {
+            Personagem personagemBuscado = BuscarPorId(idPersonagem);
+
+            if (personagemBuscado != null)
+            {
+                personagemBuscado.IdClasse = novoPersonagem.IdClasse;
+                personagemBuscado.Nome = novoPersonagem.Nome;
+                personagemBuscado.CapVida = novoPersonagem.CapVida;
+                personagemBuscado.CapMana = novoPersonagem.CapMana;
+                personagemBuscado.DataAtt = novoPersonagem.DataAtt;
+                personagemBuscado.DataCriacao = novoPersonagem.DataCriacao;
+            }
+
+            ctx.Personagems.Update(personagemBuscado);
+            ctx.SaveChanges();
+        }
+
+        public Personagem BuscarPorId(int id)
+        {
+            return ctx.Personagems.Include(x => x.IdClasseNavigation).FirstOrDefault(p => p.IdPersonagem == id);
+        }
+
+        public void Deletar(int id)
+        {
+            Personagem personagemBuscado = BuscarPorId(id);
+
+            if (personagemBuscado != null)
+            {
+                ctx.Personagems.Remove(personagemBuscado);
+                ctx.SaveChanges();
+            }
+        }
+
+        public void Inserir(Personagem personagem)
+        {
+            ctx.Personagems.Add(personagem);
+            ctx.SaveChanges();
+        }
+
+        public List<Personagem> ListarTodos()
+        {
+            return ctx.Personagems.Include(c => c.IdClasseNavigation).ToList();
+        }
+    }
+}
