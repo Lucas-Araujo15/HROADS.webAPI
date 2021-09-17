f45f0a68fa9ec89e9f3c34c7facb560c72b18332
Lucas Araujo <lucasaraujo4188@gmail.com>
2021-09-17 10:47:40 -0300

    Adiciona documentação do projeto

--- Back-end/senai.hroads.webAPI/senai.hroads.webAPI/Startup.cs
+++ Back-end/senai.hroads.webAPI/senai.hroads.webAPI/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -23,6 +24,18 @@ namespace senai.hroads.webAPI
                 .AddNewtonsoftJson(options =>
                 {
                     options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+                });
+
+            services.AddSwaggerGen(
+
+                c =>
+                {
+                    c.SwaggerDoc("v1", new OpenApiInfo
+                    {
+                        Version = "v1",
+                        Title = "senai.hroads.webAPI",
+
+                    });
                 });
 
             services.AddAuthentication(options =>
@@ -59,6 +72,14 @@ namespace senai.hroads.webAPI
             }
 
             app.UseRouting();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "senai.hroads.webAPI");
+                c.RoutePrefix = string.Empty;
+            });
 
             app.UseAuthentication();
 
