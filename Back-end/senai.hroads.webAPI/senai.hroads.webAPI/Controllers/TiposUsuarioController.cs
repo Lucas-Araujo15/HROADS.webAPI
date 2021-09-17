1416479fb391d5f3a47c2a9029f8e32d70f97a54
Lucas Araujo <lucasaraujo4188@gmail.com>
2021-09-16 23:30:19 -0300

    Adiciona o CRUD dos tipos de usuário

---
+++ Back-end/senai.hroads.webAPI/senai.hroads.webAPI/Controllers/TiposUsuarioController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using senai.hroads.webAPI.Domains;
+using senai.hroads.webAPI.Interfaces;
+using senai.hroads.webAPI.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai.hroads.webAPI.Controllers
+{
+    [Produces("application/json")]
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TiposUsuarioController : ControllerBase
+    {
+        ITipoUsuarioRepository _TipoUsuarioRepository { get; set; }
+
+        public TiposUsuarioController()
+        {
+            _TipoUsuarioRepository = new TipoUsuarioRepository();
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_TipoUsuarioRepository.ListarTodos());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok(_TipoUsuarioRepository.BuscarPorId(id));
+        }
+
+        [HttpPost]
+        public IActionResult Post(TipoUsuario tipoUsuarioCadastrar)
+        {
+            _TipoUsuarioRepository.Inserir(tipoUsuarioCadastrar);
+            return StatusCode(201);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _TipoUsuarioRepository.Deletar(id);
+            return NoContent();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, TipoUsuario novoTipoUsuario)
+        {
+            if (id == 0 || novoTipoUsuario.Titulo == null)
+            {
+                return BadRequest(
+                    new
+                    {
+                        mensagemErro = "Algumas informações não foram informadas!"
+                    }
+                );
+            }
+
+            TipoUsuario tipoUsuarioBuscado = _TipoUsuarioRepository.BuscarPorId(id);
+
+            if(tipoUsuarioBuscado != null)
+            {
+                _TipoUsuarioRepository.Atualizar(id, novoTipoUsuario);
+                return Ok();
+            }
+
+            return NotFound(
+                    new
+                    {
+                        mensagemErro = "Tipo de usuário não encontrado!",
+                        codErro = true
+                    }
+                );
+        }
+    }
+}
---
+++ Back-end/senai.hroads.webAPI/senai.hroads.webAPI/Repositories/TipoUsuarioRepository.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using senai.hroads.webAPI.Contexts;
+using senai.hroads.webAPI.Domains;
+using senai.hroads.webAPI.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai.hroads.webAPI.Repositories
+{
+    public class TipoUsuarioRepository : ITipoUsuarioRepository
+    {
+        HroadsContext ctx = new HroadsContext();
+        public void Atualizar(int idTipoUsuario, TipoUsuario novoTipoUsuario)
+        {
+            TipoUsuario tipoUsuarioBuscado = BuscarPorId(idTipoUsuario);
+
+            if (tipoUsuarioBuscado != null)
+            {
+                tipoUsuarioBuscado.Titulo = novoTipoUsuario.Titulo;
+            }
+            ctx.TipoUsuarios.Update(tipoUsuarioBuscado);
+            ctx.SaveChanges();
+        }
+
+        public TipoUsuario BuscarPorId(int id)
+        {
+            return ctx.TipoUsuarios.Include(x => x.Usuarios).FirstOrDefault(t => t.IdTipoUsuario == id);
+        }
+
+        public void Deletar(int id)
+        {
+            TipoUsuario tipoUserDelete = BuscarPorId(id);
+
+            if (tipoUserDelete != null)
+            {
+                ctx.TipoUsuarios.Remove(tipoUserDelete);
+                ctx.SaveChanges();
+            }
+        }
+
+        public void Inserir(TipoUsuario tipoUsuario)
+        {
+            ctx.TipoUsuarios.Add(tipoUsuario);
+            ctx.SaveChanges();
+        }
+
+        public List<TipoUsuario> ListarTodos()
+        {
+            return ctx.TipoUsuarios.Include(y => y.Usuarios).ToList();
+        }
+    }
+}
